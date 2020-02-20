a7a8b18207474b18723fe139e915c05b542ee5e5
Filipe Virtuoso <lfvirtuoso@gmail.com>
2020-02-20 13:29:43 -0300

    Adicionar products

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import "./App.css";
 // Components
 import Navbar from "./Components/Navbar/Navbar";
-import MainContainer from "./Components/MainContainer/MainContainer";
 import Footer from "./Components/Footer/Footer";
 import Products from "./Components/Products/Products";
 import Search from "./Components/Search/Search";
@@ -16,43 +15,55 @@ class App extends Component {
         {
           id: 1,
           name: "item1",
-          imgLink: "",
-          description: "teste",
+          imgLink:
+            "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages-assets.nasa.gov%2Fimage%2Fs48-05-024%2Fs48-05-024~thumb.jpg",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, maxime.",
           price: "10.00"
         },
         {
           id: 2,
           name: "item1",
-          imgLink: "",
-          description: "teste",
+          imgLink:
+            "https://files.slack.com/files-pri/TLAVDH7C2-FU91EDCBX/080314-space-vanguard-hsmall-1130a.grid-6x2.jpg",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, maxime.",
           price: "10.00"
         },
         {
           id: 3,
           name: "item1",
-          imgLink: "",
-          description: "teste",
+          imgLink:
+            "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages-assets.nasa.gov%2Fimage%2Fsts088-349-006%2Fsts088-349-006~thumb.jpg",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, maxime.",
           price: "10.00"
         },
         {
           id: 4,
           name: "item1",
-          imgLink: "",
-          description: "teste",
+          imgLink:
+            "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages-assets.nasa.gov%2Fimage%2Fsts088-354-031%2Fsts088-354-031~thumb.jpg",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, maxime.",
           price: "10.00"
         },
         {
           id: 5,
           name: "item1",
-          imgLink: "",
-          description: "teste",
+          imgLink:
+            "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages-assets.nasa.gov%2Fimage%2Fsts077-711-039%2Fsts077-711-039~thumb.jpg",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, maxime.",
           price: "10.00"
         },
         {
           id: 6,
           name: "item1",
-          imgLink: "",
-          description: "teste",
+          imgLink:
+            "https://files.slack.com/files-pri/TLAVDH7C2-FU91EDHHB/article-migration-image-black-knight-satellite-mystery-decoded-768x432.jpg",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, maxime.",
           price: "10.00"
         }
       ]
@@ -63,10 +74,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        {/* <Search></Search> */}
-        <MainContainer>
-          <Products products={this.state.products} />
-        </MainContainer>
+        <Products products={this.state.products} />
         <Footer />
       </div>
     );
--- ecommerce/src/Components/Products/Products.js
+++ ecommerce/src/Components/Products/Products.js
@@ -4,15 +4,60 @@ import styled from "styled-components";
 const Section = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-template-row: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-gap: 10px;
+  margin: 0.6rem;
   width: 100%;
 `;
 
-// const Article = styled.article`
-//   height: 100px
-//   width: 30%;
-//   border: 1px solid #444;
-// `;
+const Main = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-gap: 10px;
+  margin: 0.6rem;
+  text-align: center;
+`;
+
+const ItemTitle = styled.h3`
+  padding: 0.5rem;
+  // border-bottom: 2px solid #444;
+  // display: inline-block;
+  background: #444;
+  color: #f4f4f4;
+`;
+
+const Article = styled.article`
+  border: 1px dotted #444;
+  border-radius: 5px;
+  padding: 1rem;
+  background: #fafdff;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 300px;
+  border: 1px solid #444;
+`;
+
+const Div = styled.div`
+  border-radius: 5px;
+  background: #fafaed;
+  padding: 0.7rem;
+`;
+
+const Button = styled.button`
+  color: #f4f4f4;
+  background-color: #48a7f0;
+  padding: 0.5rem 2rem;
+  border: none;
+  outline: 0;
+  border-radius: 5px;
+  text-transform: uppercase;
+  font-weight: bold;
+  margin: 1rem;
+  cursor: pointer;
+`;
 
 class Products extends Component {
   constructor(props) {
@@ -21,7 +66,25 @@ class Products extends Component {
   }
 
   render() {
-    return <Section></Section>;
+    const showProducts = this.props.products.map((el, index) => {
+      return (
+        <Article>
+          <Img src={el.imgLink} alt="Imagem de um satélite" />
+          <ItemTitle>
+            Satelite {el.id} - R$ {el.price}
+          </ItemTitle>
+          <hr></hr>
+          <Div>
+            <p>{el.description}</p>
+            <Button>
+              <i class="fas fa-cart-plus"></i> Adicionar ao carrinho
+            </Button>
+          </Div>
+        </Article>
+      );
+    });
+
+    return <Main>{showProducts}</Main>;
   }
 }
 
